35b8e08e01ed5883d1647cf8c8ee1de50f2c049b
mislavskii <jornjat@gmail.com>
2024-07-30 00:18:48 +0700

    refactored gui creation, implemented logic

--- calc.py
+++ calc.py
@@ -7,14 +7,55 @@ X0 = 83
 BUTTON_SPACE = 50
 ENTRY_WIDTH = 30
 
+
+def get_values():
+    return map(
+        float, (number1_entry.get(), number2_entry.get())
+    )
+
+
+def output_result(result):
+    result_entry.delete(0, 'end')
+    result_entry.insert(0, result)
+
+
+def add():
+    num1, num2 = get_values()
+    output_result(num1 + num2)
+
+
+def sub():
+    num1, num2 = get_values()
+    output_result(num1 - num2)
+
+
+def mul():
+    num1, num2 = get_values()
+    output_result(num1 * num2)
+
+
+def div():
+    num1, num2 = get_values()
+    output_result(num1 / num2)
+
+
 window = tk.Tk()
 window.title('Calculator')
 window.geometry('350x350')
 window.resizable(False, False)
 
-buttons = [tk.Button(window, text=action, **BUTTON_PARAMS) for action in ACTIONS]
-[button.place(x=(X0 + BUTTON_SPACE * i), y=Y) for button, i in zip(buttons, range(len(ACTIONS)))]
+# buttons = [tk.Button(window, text=action, **BUTTON_PARAMS) for action in ACTIONS]
+# [button.place(x=(X0 + BUTTON_SPACE * i), y=Y) for button, i in zip(buttons, range(len(ACTIONS)))]
 # map(lambda button, i: button.place(x=(X0 + BUTTON_SPACE * i), y=Y), buttons, range(len(ACTIONS)))
+
+button_add = tk.Button(window, text='+', command=add, **BUTTON_PARAMS)
+button_add.place(x=X0, y=Y)
+button_sub = tk.Button(window, text='-', command=sub, **BUTTON_PARAMS)
+button_sub.place(x=X0 + BUTTON_SPACE, y=Y)
+button_mul = tk.Button(window, text='*', command=mul, **BUTTON_PARAMS)
+button_mul.place(x=X0 + BUTTON_SPACE * 2, y=Y)
+button_div = tk.Button(window, text='/', command=div, **BUTTON_PARAMS)
+button_div.place(x=X0 + BUTTON_SPACE * 3, y=Y)
 
 number1_entry = tk.Entry(window, width=ENTRY_WIDTH)
 number1_entry.place(x=X0, y=75)
@@ -30,14 +71,5 @@ result_entry = tk.Entry(window, width=ENTRY_WIDTH)
 result_entry.place(x=X0, y=300)
 result_label = tk.Label(window, text='Result:')
 result_label.place(x=X0, y=275)
-
-# button_add = tk.Button(window, text='+', **BUTTON_PARAMS)
-# button_add.place(x=X0, y=Y)
-# button_sub = tk.Button(window, text='-', **BUTTON_PARAMS)
-# button_sub.place(x=X0+BUTTON_SPACE, y=Y)
-# button_mul = tk.Button(window, text='*', **BUTTON_PARAMS)
-# button_mul.place(x=X0+BUTTON_SPACE*2, y=Y)
-# button_div = tk.Button(window, text='/', **BUTTON_PARAMS)
-# button_div.place(x=X0+BUTTON_SPACE*3, y=Y)
 
 window.mainloop()
