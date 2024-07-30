9fd714b5307ccaf496a4dbe8c3b81fe3ffab2ccc
mislavskii <jornjat@gmail.com>
2024-07-31 01:31:16 +0700

    refactored button creation back to list comprehension, made ACTIONS a dict

--- calc.py
+++ calc.py
@@ -1,12 +1,5 @@
 import tkinter as tk
 
-ACTIONS = ('+', '-', '*', '/')
-BUTTON_PARAMS = {'width': 3, 'height': 2}
-Y = 200
-X0 = 83
-BUTTON_SPACE = 50
-ENTRY_WIDTH = 30
-
 
 def get_values():
     return map(
@@ -39,23 +32,30 @@ def div():
     output_result(num1 / num2)
 
 
+ACTIONS = {'+': add, '-': sub, '*': mul, '/': div}
+BUTTON_PARAMS = {'width': 3, 'height': 2}
+Y = 200
+X0 = 83
+BUTTON_SPACE = 50
+ENTRY_WIDTH = 30
+
 window = tk.Tk()
 window.title('Calculator')
 window.geometry('350x350')
 window.resizable(False, False)
 
-# buttons = [tk.Button(window, text=action, **BUTTON_PARAMS) for action in ACTIONS]
-# [button.place(x=(X0 + BUTTON_SPACE * i), y=Y) for button, i in zip(buttons, range(len(ACTIONS)))]
+buttons = [tk.Button(window, text=action, command=ACTIONS[action], **BUTTON_PARAMS) for action in ACTIONS]
+[button.place(x=(X0 + BUTTON_SPACE * i), y=Y) for button, i in zip(buttons, range(len(ACTIONS)))]
 # map(lambda button, i: button.place(x=(X0 + BUTTON_SPACE * i), y=Y), buttons, range(len(ACTIONS)))
 
-button_add = tk.Button(window, text='+', command=add, **BUTTON_PARAMS)
-button_add.place(x=X0, y=Y)
-button_sub = tk.Button(window, text='-', command=sub, **BUTTON_PARAMS)
-button_sub.place(x=X0 + BUTTON_SPACE, y=Y)
-button_mul = tk.Button(window, text='*', command=mul, **BUTTON_PARAMS)
-button_mul.place(x=X0 + BUTTON_SPACE * 2, y=Y)
-button_div = tk.Button(window, text='/', command=div, **BUTTON_PARAMS)
-button_div.place(x=X0 + BUTTON_SPACE * 3, y=Y)
+# button_add = tk.Button(window, text='+', command=add, **BUTTON_PARAMS)
+# button_add.place(x=X0, y=Y)
+# button_sub = tk.Button(window, text='-', command=sub, **BUTTON_PARAMS)
+# button_sub.place(x=X0 + BUTTON_SPACE, y=Y)
+# button_mul = tk.Button(window, text='*', command=mul, **BUTTON_PARAMS)
+# button_mul.place(x=X0 + BUTTON_SPACE * 2, y=Y)
+# button_div = tk.Button(window, text='/', command=div, **BUTTON_PARAMS)
+# button_div.place(x=X0 + BUTTON_SPACE * 3, y=Y)
 
 number1_entry = tk.Entry(window, width=ENTRY_WIDTH)
 number1_entry.place(x=X0, y=75)
